7a91b258137668a770169ef583ba45cff09263f9
melonmochi <melonmochi3@gmail.com>
2019-11-08 18:33:24 +0100

    Deleted excel's index column

--- extractfiles.py
+++ extractfiles.py
@@ -4,32 +4,23 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-zip_dir = input(r"Insertar path completo del zip(incluído el nombre del fichero): ")
+zip_dir = input(
+    r"Insertar path completo del zip(incluído el nombre del fichero): ")
 path, filename = os.path.split(zip_dir)
-file = path + '\\' + filename + ".zip"
+zip_file = path + '\\' + filename + ".zip"
 newfolder = path + '\\' + filename
 outputexcel = path + '\\' + filename + ".xlsx"
 
-print(path)
-print(file)
-print(newfolder)
-print(outputexcel)
-
-with zipfile.ZipFile(file) as zip_ref:
+with zipfile.ZipFile(zip_file) as zip_ref:
     zip_ref.extractall(newfolder)
 
-onlyfiles = [f for f in listdir(newfolder) if isfile(join(newfolder, f))]
-print(onlyfiles)
+files_to_insert = [
+    f for f in listdir(newfolder)
+    if isfile(join(newfolder, f)) and f.endswith('.txt')
+]
 
-for filename in listdir(newfolder):
-    if filename.endswith(".txt"):
-        gtfsfile = newfolder+'\\'+filename
+with pd.ExcelWriter(outputexcel) as writer:
+    for filename in files_to_insert:
+        gtfsfile = newfolder + '\\' + filename
         df = pd.read_csv(gtfsfile)
-        df.to_excel(outputexcel, sheet_name=filename)
-        continue
-    else:
-        continue
-
-#df = pd.read_csv(newfolder)
-#print(df)
-#df.to_excel(outputexcel, 'Sheet1')
+        df.to_excel(writer, sheet_name=filename, index=False)
