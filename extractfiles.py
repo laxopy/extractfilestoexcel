631261513bd71045fb7b9ec32a4ab3a2a886fe6f
laxopy <jlaxlopez@gmail.com>
2019-11-11 16:26:01 +0100

    Adapt path management to any OS

--- extractfiles.py
+++ extractfiles.py
@@ -1,26 +1,27 @@
 import pandas as pd
 import zipfile
-import os
 from os import listdir
 from os.path import isfile, join
+from pathlib import Path
 
 zip_dir = input(
-    r"Insertar path completo del zip(incluído el nombre del fichero): ")
-path, filename = os.path.split(zip_dir)
-zip_file = path + '\\' + filename + ".zip"
-newfolder = path + '\\' + filename
-outputexcel = path + '\\' + filename + ".xlsx"
+    r"Arrastrar el archivo que quieres exportar, o copia su ruta completa aquí: ")
+file_path = Path(zip_dir)
+file_name = file_path.stem
+new_folder = file_path.parents[0] / file_name
+output_excel = new_folder / file_name
+output_excel = output_excel.with_suffix('.xlsx')
 
-with zipfile.ZipFile(zip_file) as zip_ref:
-    zip_ref.extractall(newfolder)
+with zipfile.ZipFile(file_path) as zip_ref:
+    zip_ref.extractall(new_folder)
 
 files_to_insert = [
-    f for f in listdir(newfolder)
-    if isfile(join(newfolder, f)) and f.endswith('.txt')
+    f for f in listdir(new_folder)
+    if isfile(join(new_folder, f)) and f.endswith('.txt')
 ]
 
-with pd.ExcelWriter(outputexcel) as writer:
+with pd.ExcelWriter(output_excel) as writer:
     for filename in files_to_insert:
-        gtfsfile = newfolder + '\\' + filename
-        df = pd.read_csv(gtfsfile)
+        gtfs_file = new_folder / filename
+        df = pd.read_csv(gtfs_file)
         df.to_excel(writer, sheet_name=filename, index=False)
